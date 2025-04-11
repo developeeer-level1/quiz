380f31ac627a1136bf59e12488e43c6cac5d8d2c
developeeer-level1 <yak.yakovochka@gmail.com>
2025-04-11 13:46:59 +0100

    Add project files.

---
+++ quiz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] menu = {
+                                "╔════════╗",
+                                "║        ║",
+                                "╚════════╝"
+                            };
+            foreach (string line in menu)
+            {
+                int left = (Console.WindowWidth - (line.Length/2)) / 2;
+                Console.SetCursorPosition(left < 0 ? 0 : left, Console.CursorTop);
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
